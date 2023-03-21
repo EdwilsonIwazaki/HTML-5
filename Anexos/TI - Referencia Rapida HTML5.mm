b83c54d0a6d5be5525d130e2a9a5556ce4b9608a
Edwilson Iwazaki <dilso2005@gmail.com>
2023-03-20 23:47:12 -0300

    Referencia Rapida

---
+++ Anexos/TI - Referencia Rapida HTML5.mm
@@ -0,0 +1,141 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_191153586" CREATED="1610381622013" MODIFIED="1679365032148" FORMAT_AS_HYPERLINK="false" VGAP_QUANTITY="2 pt" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="8 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="file:///C:\Users\dilso\OneDrive\Documentos\TI - Referencia Rapida HTML5_files\Logo HTML5.png" alt="Logo HTML5"/>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="file:/C:/Users/dilso/OneDrive/Documentos/Dilso.mm" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_12905907" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm" VGAP_QUANTITY="2 pt" CHILD_NODES_ALIGNMENT="BY_CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_12905907" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_520072399">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_520072399"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64 pt" COLOR="#ffffff" BACKGROUND_COLOR="#0033ff" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="8 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#333333" BORDER_DASH="SOLID">
+<font NAME="Segoe Print" SIZE="22" BOLD="true"/>
+<edge COLOR="#ffffff" WIDTH="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="5 px" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_linear" COLOR="#808080" WIDTH="5"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28 px" COLOR="#000000" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="4 px" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24 px" COLOR="#000000" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<font SIZE="15"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24 px" COLOR="#111111" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="2 px" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24 px" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true" STYLE="bubble">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24 px" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16 px" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16 px" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16 px" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16 px" STYLE="bubble" BORDER_WIDTH_LIKE_EDGE="false" BORDER_COLOR_LIKE_EDGE="true">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Conceito" POSITION="right" ID="ID_273436263" CREATED="1610381622018" MODIFIED="1679365113643" VGAP_QUANTITY="2 pt" HGAP_QUANTITY="91.2683 pt" VSHIFT_QUANTITY="-30.73171 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="HTML5 é a quinta versão da linguagem de marcação Hypertext Markup Language (HTML), usada para criar e estruturar conteúdo na web. Foi lançado em 2014" ID="ID_588194298" CREATED="1610381622019" MODIFIED="1679365286953" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-12.29268 pt"/>
+</node>
+<node TEXT="TAGS" POSITION="right" ID="ID_1240655002" CREATED="1679356995943" MODIFIED="1679365372321" HGAP_QUANTITY="103.25 pt" VSHIFT_QUANTITY="-6.75 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="&lt;!DOCTYPE html&gt;" ID="ID_1110727438" CREATED="1679357012758" MODIFIED="1679365597857">
+<node TEXT="Instrução que diz qual versão do HTML estamos usando." ID="ID_1788325482" CREATED="1679357033615" MODIFIED="1679365648292"/>
+</node>
+<node FOLDED="true" ID="ID_41504673" CREATED="1679357027703" MODIFIED="1679365670739"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;html&gt;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="que indica o início e o fim do documento HTML." ID="ID_1531611116" CREATED="1679365679120" MODIFIED="1679365719255"/>
+</node>
+<node TEXT="&lt;head&gt;" ID="ID_170334471" CREATED="1679365780695" MODIFIED="1679365793302">
+<node TEXT="que contém informações sobre a página, como o título e links para arquivos externos." ID="ID_1583592021" CREATED="1679365793812" MODIFIED="1679365810130"/>
+<node TEXT="&lt;meta" FOLDED="true" ID="ID_223953948" CREATED="1679366026254" MODIFIED="1679366045207">
+<node TEXT="é usada para fornecer metadados sobre uma página da web. Não é visível para os visitantes do site. Algumas utilizações comuns da tag incluem especificar o conjunto de caracteres usado pela página da web, fornecer uma descrição da página e fornecer palavras-chave relevantes para o conteúdo da página. A tag &quot;&lt;meta&gt;&quot; também pode ser usada para fornecer outros tipos de informações, como o autor da página, a data em que a página foi criada ou modificada pela última vez e muito mais." ID="ID_82489790" CREATED="1679366352653" MODIFIED="1679366407494"/>
+</node>
+<node TEXT="&lt;title&gt;" FOLDED="true" ID="ID_1608323664" CREATED="1679366049097" MODIFIED="1679366059252">
+<node TEXT="é usada para definir o título da página da web. É exibida na barra de título do navegador quando a página é carregada. O conteúdo da tag &quot;&lt;title&gt;&quot; também é usado pelos motores de busca para indexar a página e exibir o título nos resultados da pesquisa. É importante que o título da página seja relevante para o conteúdo da página e contenha palavras-chave relevantes para ajudar a melhorar a visibilidade nos resultados de pesquisa." ID="ID_85865445" CREATED="1679366550730" MODIFIED="1679366597427"/>
+</node>
+</node>
+<node TEXT="&lt;body&gt;" ID="ID_160281487" CREATED="1679366682377" MODIFIED="1679366690858"/>
+</node>
+</node>
+</map>
